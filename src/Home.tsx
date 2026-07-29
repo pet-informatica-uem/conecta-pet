b12f5fc3f6d18b0117c63178d12bcc7f52698f7c
Petiano Mestre do PET Informática UEM <pet@din.uem.br>
2026-07-29 20:22:56 -0300

    Reorder Maratona component in Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -15,8 +15,8 @@ export default function Home() {
       <main>
         <Hero />
         <SobreEvento />
-        <Maratona />
         <ApresentacaoTrabalhos />
+        <Maratona />
         <Cronograma />
       </main>
       <Footer />
